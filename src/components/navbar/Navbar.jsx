c3a638489844ce61994edc32546f4e8f890451c0
hieuhp132 <hieuhp132@gmail.com>
2025-12-05 19:34:33 +0700

    restructure, fix missmatch filenames

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import { useI18n } from "../../i18n";
-import "./NavBar.css";
+import "./Navbar.css";
 
 import {
   FaSearch,
